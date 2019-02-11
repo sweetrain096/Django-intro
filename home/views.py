e75da4300d8d8380e29c90f540fb641b1bad8fd4
sweetrain096 <gpfhddl09@gmail.com>
2019-02-11 04:52:11 +0000

    04_template iniheritance/for/if

--- home/views.py
+++ home/views.py
@@ -16,6 +16,7 @@ def dinner(request):
     # 1) request, 2) template 파일(html)
     # render 선택인자.
     # 3) dictionary : 템플릿에서 쓸 변수 값을 정의
-    return render(request, "dinner.html", {'dinner' : pick})
+    return render(request, "dinner.html", {'dinner' : pick, 'box' : box})
+    # return ('dinner.html', dinner = dinner, box = box)
     # template은 기본적으로 문법이 jinja2와 같은데(비슷), 장고에서는 DTL을 쓴다.
     # Django Template Language
