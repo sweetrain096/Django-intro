8b4473a39d177d807e101270feaad729258c4297
sweetrain096 <gpfhddl09@gmail.com>
2019-02-11 02:49:50 +0000

    README

--- home/views.py
+++ home/views.py
@@ -10,12 +10,12 @@ def index(request):
     
 def dinner(request):
     box = ["치킨", "충만치킨", "엽떡", "초밥"]
-    dinner = random.choice(box)
-    # return HttpResponse(f"오늘 저녁은 {dinner}!!")
+    pick = random.choice(box)
+    # return HttpResponse(f"오늘 저녁은 {pick}!!")
     # render 필수인자.
     # 1) request, 2) template 파일(html)
     # render 선택인자.
     # 3) dictionary : 템플릿에서 쓸 변수 값을 정의
-    return render(request, "dinner.html", {'dinner' : dinner})
+    return render(request, "dinner.html", {'dinner' : pick})
     # template은 기본적으로 문법이 jinja2와 같은데(비슷), 장고에서는 DTL을 쓴다.
     # Django Template Language
